c1c1a90d1e3d6761cf9c5eec9bfe17265cc1ecbc
tuhfatuljamilah <67049488+tuhfatuljamilah@users.noreply.github.com>
2020-07-02 15:28:19 +0700

    Create style.CSS

---
+++ style.CSS
@@ -0,0 +1,53 @@
+body{
+    background: #F2F2F2;
+    font-family: sans-serif;
+}
+.kalkulator{
+    width: 300px;
+    background: #FFA800;
+    padding: 10px 20px 30px 20px;
+    border-radius: 5px;
+    box-shadow: 0px 10px 10px 0px #D1D1D1;
+}
+.bil{
+    width: 280px;
+    margin: 10px;
+    border: none;
+    font-size: 14pt;
+    border-radius: 5px;
+    padding: 10px; 
+}
+.opt{
+    font-size: 14pt;
+    border: none;
+    width: 280px;
+    margin: 10px;
+    border-radius: 5px;
+    padding: 10px;
+}
+.tombol{
+    background: #A9A9A9;
+    margin: 10px;
+    border-top: none;
+    border-right: none;
+    border-left: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    color: #eee;
+    font-size: 15pt;
+    border-bottom:2px solid #64950;
+}
+.hasil{
+    width: 280px;
+    margin: 10px;
+    border: none;
+    font-size: 14pt;
+    border-radius: 5px;
+    padding: 10px; 
+    background: #eee;
+} 
+.judul{
+text-align: center;
+color: #eee;
+font-weight: 300px;
+}
